400e03b50e10b25ab04ec2219fbc1980d3e33a5c
Jacob Budzis <jrbudzis@gmail.com>
2025-08-17 01:42:55 -0700

    Implement file selection

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "Example Application",
+        "sub2srs",
         options,
         Box::new(|_cc| Ok(Box::<MyApp>::default())),
     )
@@ -16,14 +16,23 @@ fn main() -> eframe::Result {
 
 #[derive(Default)]
 struct MyApp {
-    dropped_files: Vec<egui::DroppedFile>,
     picked_path: Option<String>,
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Hello world!");
+            if ui.button("Select video...").clicked()
+                && let Some(path) = rfd::FileDialog::new().pick_file()
+            {
+                self.picked_path = Some(path.display().to_string());
+            }
+            if let Some(picked_path) = &self.picked_path {
+                ui.horizontal(|ui| {
+                    ui.label("Picked file:");
+                    ui.monospace(picked_path);
+                });
+            }
         });
     }
 }
